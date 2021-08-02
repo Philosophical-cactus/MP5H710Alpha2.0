ec55dda50cc3165e308e4c7166e08001290b7d0c
Philosophical_cactus <70976201+Philosophical-cactus@users.noreply.github.com>
2021-08-01 19:53:01 -0500

    Add files via upload

---
+++ MP5H710.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using Gungeon;
+using ItemAPI;
+using MonoMod;
+using UnityEngine;
+
+namespace MP5H710
+{
+
+    public class MP5H710 : GunBehaviour
+    {
+        public static void Add()
+        {
+            Gun gun = ETGMod.Databases.Items.NewGun("MP5H710", "MP5H710");
+            Gun gun2 = PickupObjectDatabase.GetById(84) as Gun;
+            gun.shellCasing = gun2.shellCasing;
+            Game.Items.Rename("outdated_gun_mods:mp5h710", "blre:mp5h710");
+            gun.gameObject.AddComponent<MP5H710>();
+            gun.SetShortDescription("Simple yet Elegant");
+            gun.SetLongDescription("The MP5H710 was deployed often with higher ranking millitary officers. It may not have specialized attatchments or capabilities, but it does one thing very well...    ...Kill");
+            gun.SetupSprite(null, "MP5H710_idle_001", 8);
+            gun.SetAnimationFPS(gun.shootAnimation, 24);
+            gun.SetAnimationFPS(gun.reloadAnimation, 21);
+            gun.carryPixelOffset = new IntVector2(6, -3);
+            gun.AddProjectileModuleFrom(PickupObjectDatabase.GetById(519) as Gun, true, false);
+            
+
+
+            gun.DefaultModule.ammoCost = 1;
+            gun.DefaultModule.shootStyle = ProjectileModule.ShootStyle.Automatic;
+            gun.DefaultModule.sequenceStyle = ProjectileModule.ProjectileSequenceStyle.Random;
+            gun.reloadTime = 1.3f;
+            gun.DefaultModule.cooldownTime = 0.1f;
+            gun.DefaultModule.numberOfShotsInClip = 15;
+            gun.SetBaseMaxAmmo(250);
+            gun.quality = PickupObject.ItemQuality.B;
+            gun.encounterTrackable.EncounterGuid = "Rattled!";
+
+
+
+            Gun yes = (PickupObjectDatabase.GetById(519) as Gun);
+            Projectile projectile = UnityEngine.Object.Instantiate<Projectile>(yes.DefaultModule.projectiles[0]);
+            projectile.gameObject.SetActive(false);
+            FakePrefab.MarkAsFakePrefab(projectile.gameObject);
+            UnityEngine.Object.DontDestroyOnLoad(projectile);
+            gun.DefaultModule.projectiles[0] = projectile;
+            projectile.baseData.damage = 3.0f;
+            projectile.baseData.speed *= 2f;
+            projectile.transform.parent = gun.barrelOffset;
+            ETGMod.Databases.Items.Add(gun, null, "ANY");
+        }
+
+        public override void OnPostFired(PlayerController player, Gun gun)
+        {
+            //This determines what sound you want to play when you fire a gun.
+            //Sounds names are based on the Gungeon sound dump, which can be found at EnterTheGungeon/Etg_Data/StreamingAssets/Audio/GeneratedSoundBanks/Windows/sfx.txt
+            gun.PreventNormalFireAudio = false;
+            AkSoundEngine.PostEvent("Play_WPN_elephantgun_shot_01", gameObject);
+        }
+        private bool HasReloaded;
+        //This block of code allows us to change the reload sounds.
+        protected void Update()
+        {
+            if (gun.CurrentOwner)
+            {
+
+                if (!gun.PreventNormalFireAudio)
+                {
+                    this.gun.PreventNormalFireAudio = true;
+                }
+                if (!gun.IsReloading && !HasReloaded)
+                {
+                    this.HasReloaded = true;
+                }
+            }
+        }
+
+        public override void OnReloadPressed(PlayerController player, Gun gun, bool bSOMETHING)
+        {
+            if (gun.IsReloading && this.HasReloaded)
+            {
+                HasReloaded = false;
+                AkSoundEngine.PostEvent("Stop_WPN_All", base.gameObject);
+                base.OnReloadPressed(player, gun, bSOMETHING);
+                AkSoundEngine.PostEvent("Play_WPN_dartgun_reload_01", base.gameObject);
+            }
+        }
+
+        //All that's left now is sprite stuff. 
+        //Your sprites should be organized, like how you see in the mod folder. 
+        //Every gun requires that you have a .json to match the sprites or else the gun won't spawn at all
+        //.Json determines the hand sprites for your character. You can make a gun two handed by having both "SecondaryHand" and "PrimaryHand" in the .json file, which can be edited through Notepad or Visual Studios
+        //By default this gun is a one-handed weapon
+        //If you need a basic two handed .json. Just use the jpxfrd2.json.
+        //And finally, don't forget to add your Gun to your ETGModule class!
+    }
+}
